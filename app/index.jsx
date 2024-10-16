3adb1e3d7943623910c78a983d8ee16aed03a010
chocopndn <chocopndn@gmail.com>
2024-10-17 01:32:30 +0800

    UPDATE: Splash screen

--- app/index.jsx
+++ app/index.jsx
@@ -2,17 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "react-native";
+
+import images from "../constants/images";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Link href="./(auth)/signin" asChild>
         <Pressable>
-          <Text className="text-center">Logo</Text>
-          <Text className="text-center">HYD</Text>
-          <Text className="text-center">How's Your Day?</Text>
-          <View className="mt-3">
-            <Text className="text-center">YOU MATTER</Text>
+          <View className="flex-1 items-center">
+            <View className="flex-1 items-center justify-center">
+              <Image source={images.logo} className="h-40 w-40" />
+              <Text className="text-center font-ibold text-3xl">HYD</Text>
+              <Text className="text-center text-2xl font-iregular">
+                How's Your Day?
+              </Text>
+            </View>
+            <View className="mb-20">
+              <Text className="text-center text-2xl font-iregular">
+                YOU MATTER
+              </Text>
+            </View>
           </View>
         </Pressable>
       </Link>
